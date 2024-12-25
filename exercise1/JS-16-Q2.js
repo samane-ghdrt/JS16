2acc32b5632a956565f26973edec3f662f925413
samane ghodrati <samane.ghodrati@gmail.com>
2024-12-25 00:03:34 -0900

    add

--- exercise1/JS-16-Q2.js
+++ exercise1/JS-16-Q2.js
@@ -1,3 +1,8 @@
 //? Question-2:Write a code that, upon clicking a button labeled "Place Bid" changes its text to "Added to Cart" and disables the button (you can modify the style if necessary).
 // hint: watch help.mp4
 //!Answer:
+const button=document.querySelector('section button p');
+button.textContent
+button.addEventListener('click',function(e){
+e.target.textContent="Added to Cart";
+})
--- exercise1/JS-16-Q3.js
+++ exercise1/JS-16-Q3.js
@@ -1,3 +1,9 @@
 // ? Qustion-3:Write a code to close message box when close button clicked :
 // hint: watch help.mp4
 //! answer
+const messageBox = document.getElementById("myMessageBox");
+const closeButton = document.getElementById("");
+
+closeButton.addEventListener("click", function() {
+  messageBox.style.display = "none";
+});
