759a7a946050f166652496d0307edf2b21e00796
samane ghodrati <samane.ghodrati@gmail.com>
2024-12-28 22:48:36 -0900

    add

--- exercise1/JS-16-Q4.js
+++ exercise1/JS-16-Q4.js
@@ -1,10 +1,11 @@
 // ? Qustion-4:Write a code that changes the background color of the stars when we click on them.
 // hint: watch help.mp4
 //! answer
-const starsEl= document.querySelectorAll('.stars i');
+const starsEl= document.querySelectorAll('.fa-star');
+console.log(starsEl);
 
-starsel.forEach(star => {
+starsEl.forEach(star => {
 
 star.addEventListener('click',(e)=>{
-e.target.classlist.toggle('.solid');})
+star.classlist.toggle('solid');})
 })
