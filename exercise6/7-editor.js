a135064083a8536a7d1d541b0ccc35a4ead7f662
samane <samane.ghodrati@gmail.com>
2024-12-30 12:53:18 -0800

    add

--- exercise6/7-editor.js
+++ exercise6/7-editor.js
@@ -6,3 +6,33 @@ Look at editor.png, you should create a text editor similar to that.
 - The third button bolds the text when clicked.
 - The fourth button italicizes the text when clicked.
 */
+const reoveEl=document.querySelector('.remove');
+reoveEl.addEventListener('click',()=>{
+    let textElement = document.querySelector(".input");
+    let currentText = textElement.value;
+  
+    if (currentText.length > 0) {
+      let newText = currentText.slice(0, -1); // Remove the last character
+      textElement.value= newText;
+    }  
+})
+const deleteEl=document.querySelector('.delete');
+deleteEl.addEventListener('click',()=>{
+    let textElement = document.querySelector(".input");
+    let currentText = textElement.value;
+  
+    if (currentText.length > 0) {
+      let newText = ""; 
+      textElement.value= newText;
+    }  
+})
+const boldEl=document.querySelector('.bold');
+boldEl.addEventListener('click',()=>{
+    let textElement = document.querySelector(".input");
+    let currentText = textElement.value;
+  
+    if (currentText.length > 0) {
+      let newText = currentText.style.fontWeight= "bolder"; 
+      textElement.value= newText;
+    }  
+})
